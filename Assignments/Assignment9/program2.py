0bb5da8e645ed48e641897d48edf26ffc7670d47
ganeshitkale36 <ganeshitkale36@gmail.com>
2026-02-04 06:32:56 +0530

    Add function to check and print greater of two numbers

---
+++ Assignments/Assignment9/program2.py
@@ -0,0 +1,20 @@
+
+def ChkGreater(no1,no2):
+    value1 = 10
+    value2 = 20
+
+    if(value1<value2):
+        print("No is greater:",value2)
+    else:
+        print("No is not  greater:",value2)
+
+def main():
+    ChkGreater(10,20)
+
+if __name__=="__main__":
+    main()
+
+    
+   
+    
+
