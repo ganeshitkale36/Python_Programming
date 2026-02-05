7ed52a9beed5f9604d4dea5104d8779325975c8b
ganeshitkale36 <ganeshitkale36@gmail.com>
2026-02-06 04:54:06 +0530

    Add rectangle area calculation program

---
+++ Assignments/Assignment13/program1.py
@@ -0,0 +1,8 @@
+def main():
+    length = int(input("Enter a length:"))
+    width = int(input("Enter a width:"))
+    Area = length * width
+    print("Area of rectangle is :",Area)
+
+if __name__=="__main__":
+    main()
