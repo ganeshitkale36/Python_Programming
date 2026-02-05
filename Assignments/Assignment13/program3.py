461a4505f0a659afaac32b66f94339823f89b5c1
ganeshitkale36 <ganeshitkale36@gmail.com>
2026-02-06 04:57:18 +0530

    Add even number check program

---
+++ Assignments/Assignment13/program3.py
@@ -0,0 +1,17 @@
+def main():
+
+    No = int(input("Enter a number:"))
+    if (No % 2 == 0):
+        print("Number is perfect:",No)
+    else:
+        print("Number is not perfect",No)
+
+
+if __name__=="__main__":
+    main()
+    
+ 
+
+
+
+ 
