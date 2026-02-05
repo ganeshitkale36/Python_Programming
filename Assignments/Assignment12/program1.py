24228824828740c1a7cc4cab1faf16e893dfdca0
ganeshitkale36 <ganeshitkale36@gmail.com>
2026-02-05 09:21:41 +0530

    Add program to check whether a character is a vowel or consonant

---
+++ Assignments/Assignment12/program1.py
@@ -0,0 +1,10 @@
+def main():
+    
+    ch = input("Enter a character")  
+    if ch in ['a','e','i','o','u']:
+        print("It is vowel",ch)
+    else:
+        print("It is constant",ch )
+
+if __name__=="__main__":
+    main() 
