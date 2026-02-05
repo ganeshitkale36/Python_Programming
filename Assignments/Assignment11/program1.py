c8cf23fb059dfbd4f6fb118041c11d01b07f4732
ganeshitkale36 <ganeshitkale36@gmail.com>
2026-02-05 09:07:51 +0530

    Fix prime number check logic

---
+++ Assignments/Assignment11/program1.py
@@ -0,0 +1,9 @@
+def main():
+    Num  = int(input("Enter a number:"))
+    if (Num % 1 == 0):
+        print("Number is prime")
+    else:
+        print("Number is not prime")
+
+if __name__=="__main__":
+    main()
