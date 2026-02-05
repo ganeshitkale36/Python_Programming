3ec119829312fb32e34ca07e233f572d1e79934f
ganeshitkale36 <ganeshitkale36@gmail.com>
2026-02-05 09:25:18 +0530

    Add program to perform basic arithmetic operations (add, subtract, multiply, divide)

---
+++ Assignments/Assignment12/program3.py
@@ -0,0 +1,34 @@
+def main():
+    No1 = int(input("Enter a first number:"))
+    No2 = int(input("Enter a second number:"))
+    Ans = 0
+    Ans = No1 + No2
+    print("Addition is :",Ans)
+
+
+    No1 = int(input("Enter a first number:"))
+    No2 = int(input("Enter a second number:"))
+    Ans = 0
+    Ans = No1 - No2
+    print("Substraction is :",Ans)
+
+
+
+    No1 = int(input("Enter a first number:"))
+    No2 = int(input("Enter a second number:"))
+    Ans = 0
+    Ans = No1 * No2
+    print("Multipication is :",Ans)
+
+
+
+    No1 = int(input("Enter a first number:"))
+    No2 = int(input("Enter a second number:"))
+    Ans = 0
+    Ans = No1 / No2
+    print("Division is :",Ans)
+
+
+
+if __name__=="__main__":
+    main()
