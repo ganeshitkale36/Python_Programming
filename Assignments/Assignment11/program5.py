21fde2238fb9dabded2c401412b74243f21ff9be
ganeshitkale36 <ganeshitkale36@gmail.com>
2026-02-05 09:18:41 +0530

    Add program to check whether a number is palindrome

---
+++ Assignments/Assignment11/program5.py
@@ -0,0 +1,19 @@
+def palidrome(Number):
+     
+
+    (Number[::-1])
+         
+         
+def main():
+
+     No = input("Enter a number:")
+     Ret = 1
+     Ret = palidrome(No)
+     print("Number is palidrome:",Ret)
+    
+
+
+if __name__=="__main__":
+     main()
+    
+
