fe409d7d383ab2a85e2b4ed4c0454990af12d06c
ganeshitkale36 <ganeshitkale36@gmail.com>
2026-02-07 11:25:20 +0530

    Add Python program to find maximum of two numbers using lambda function

---
+++ Assignments/Assignment14/program3.py
@@ -0,0 +1,20 @@
+chk = lambda Value1,Value2:(Value1>Value2)
+       
+       
+def main():
+    value1 = int(input("Enter a first  Number:"))      
+    value2 = int(input("Enter a  second Number:"))
+  
+    Ret = 0
+    Ret = chk(value1,value2)
+
+    if(Ret == True):
+        print("Max no is:",value1)
+
+    else:
+        print("Max no is:",value2)
+
+
+   
+if __name__=="__main__":
+    main()
