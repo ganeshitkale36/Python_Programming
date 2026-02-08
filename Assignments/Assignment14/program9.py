957ee725df53f9afae2c7bb3ad364784b3653f38
ganeshitkale36 <ganeshitkale36@gmail.com>
2026-02-08 08:22:37 +0530

    Add lambda-based multiplication of two numbers

---
+++ Assignments/Assignment14/program9.py
@@ -0,0 +1,14 @@
+Multiplication = lambda No1,No2:(No1 * No2)
+
+def main():
+    value1 = int(input("Enter a  first number:"))
+    value2 = int(input("Enter a second number:"))
+
+   
+    Ret  = Multiplication(value1,value2)
+    print("Multiplication is:",Ret)
+    
+  
+
+if __name__=="__main__":
+    main()
