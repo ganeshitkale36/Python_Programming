4ea64f804f837115d4b60713e668a280a1d12ac5
ganeshitkale36 <ganeshitkale36@gmail.com>
2026-02-08 08:21:41 +0530

    Implement addition of two numbers using lambda function

---
+++ Assignments/Assignment14/program8.py
@@ -0,0 +1,14 @@
+Addition = lambda No1,No2:(No1 + No2)
+
+def main():
+    value1 = int(input("Enter a  first number:"))
+    value2 = int(input("Enter a second number:"))
+
+   
+    Ret  = Addition(value1,value2)
+    print("Addition is:",Ret)
+    
+  
+
+if __name__=="__main__":
+    main() 
