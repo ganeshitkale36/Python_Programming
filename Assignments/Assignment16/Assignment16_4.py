9f54b0cad87a47450a01208aaa02306fac5639c2
ganeshitkale36 <ganeshitkale36@gmail.com>
2026-02-11 23:10:37 +0530

    Add example of creating and printing multiple lists

---
+++ Assignments/Assignment16/Assignment16_4.py
@@ -0,0 +1,11 @@
+Name1 = ["Marvellous"]
+Name2 = ["Marvellous"]
+Name3 = ["Marvellous"]
+Name4 = ["Marvellous"]
+Name5 = ["Marvellous"]
+
+print(Name1)
+print(Name2)
+print(Name3)
+print(Name4)
+print(Name5)
