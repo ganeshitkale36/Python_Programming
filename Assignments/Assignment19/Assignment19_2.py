25e7b9139d1a91ff05acf32b0259708cac40faa2
ganeshitkale36 <ganeshitkale36@gmail.com>
2026-02-16 22:44:38 +0530

    Add multiplication script using lambda

---
+++ Assignments/Assignment19/Assignment19_2.py
@@ -0,0 +1,10 @@
+multiplcation = lambda No1,No2:No1 * No2
+
+def main():
+    num1 = int(input("Enter a first Number:"))
+    num2= int(input("Enter a Second Number:"))
+    Ret = multiplcation(num1,num2)
+    print(Ret)
+
+if __name__=="__main__":
+    main()
